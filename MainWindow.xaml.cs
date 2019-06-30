6d39aa4633968f38cbf96cec80f74879da067779
jackeline varela vasquez <jackelinevarela2@gmail.com>
2019-06-30 12:22:44 -0600

    // Llenar el ListBox Se cambió: lbZoologicos.DisplayMemberPath = "nombre"; a:     lbZoologicos.DisplayMemberPath = "ciudad"; debido a que as[i estaba en la base de datos

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+//Agregar los namespaces necesarios para SQL Server
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
 
 namespace AdministradosZoologicoJacki
 {
@@ -20,9 +25,56 @@ namespace AdministradosZoologicoJacki
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Variable miembro
+        SqlConnection sqlConnection;
+
+        // Constructor
         public MainWindow()
         {
             InitializeComponent();
+
+            // Coonection String
+            string conectionString = ConfigurationManager.ConnectionStrings["AdministradosZoologicoJacki.Properties.Settings.ZoologicoConnectionString"].ConnectionString;
+            sqlConnection = new SqlConnection(conectionString);
+
+            // Llenar el ListBox de Zoologicos
+            MostrarZoologicos();
         }
+
+        // Método para poder llenar el primer ListBox
+        private void MostrarZoologicos()
+        {
+            try
+            {
+                // El query a realizar en la base de datos
+                string query = "SELECT * from Zoo.Zoologico";
+                // SQLDataAdapter es una interfaz donde las tablas y los objetos de c#
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
+
+                using (sqlDataAdapter)
+                {
+                    // Objeto en C# que refleja una tabla de una BD
+                    DataTable tablaZoologico = new DataTable();
+
+                    // Llenar el objeto de tipo DataTable
+                    sqlDataAdapter.Fill(tablaZoologico);
+
+                    // Llenar el ListBox
+                    // ¿Cúal información de la tabla en el DataTable debería ser desplegada en el ListBox?
+                    lbZoologicos.DisplayMemberPath = "ciudad";
+                    // ¿Qué valor debe ser entregado cuando un elemento de nuestro ListBox es seleccionado?
+                    lbZoologicos.SelectedValuePath = "id";
+                    //¿Quién es la referencia de los datos para el ListBox (popular)?
+                    lbZoologicos.ItemsSource = tablaZoologico.DefaultView;
+                }
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show(e.ToString());
+            }
+        }
+        
+       
     }
 }
